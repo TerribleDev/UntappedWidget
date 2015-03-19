086bd4d8ac9f5d7bf9ffa280b9635046bb19310c
Tommy Parnell <tparnell8@gmail.com>
2015-03-19 00:25:09 -0400

    use prod url

--- src/UntappedWidgetGenerator.Web/Scripts/Jquery.Untapped.js
+++ src/UntappedWidgetGenerator.Web/Scripts/Jquery.Untapped.js
@@ -2,7 +2,7 @@
     $.fn.untappd = function (username) {
         this.each(function () {
             var that = this;
-            $.post("http://localhost:12116/" + username)
+            $.post("http://untappedwidget.azurewebsites.net/" + username)
                 .success(function (data) {
                     $(that).html(data);
             });
